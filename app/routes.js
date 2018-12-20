9f79252d3c16bfa94b65c6dab7360f2ad9e50c14
hvekriya <h.vekriya@kainos.com>
2018-12-20 14:42:06 +0000

    Added in a checkbox table

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,8 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
 module.exports = router
