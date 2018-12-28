13996e4d10d4f247edf0f2ff8200f167be52d368
hvekriya <h.vekriya@kainos.com>
2018-12-28 15:42:08 +0000

    Added in report section and removed search and replaced it with office

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -21,4 +21,111 @@ module.exports = {
 
   // Insert values here
 
+  'newApplications': [{
+      'id': 'PEX1010784',
+      'name': 'Lacey Cervantes',
+      'collectionDate': '29/12/2018',
+      'collectionTime': '09:51',
+      'collectionOffice': 'Peterborough'
+    },
+    {
+      'id': 'PEX2095210',
+      'name': 'Lawrence Robertson',
+      'collectionDate': '24/12/2018',
+      'collectionTime': '12:20',
+      'collectionOffice': 'Durham'
+    },
+    {
+      'id': 'PEX3100591',
+      'name': 'Veronica Carver',
+      'collectionDate': '30/12/2018',
+      'collectionTime': '10:11',
+      'collectionOffice': 'Liverpool'
+    },
+    {
+      'id': 'PEX2297261',
+      'name': 'Olivia Fischer',
+      'collectionDate': '21/12/2018',
+      'collectionTime': '09:28',
+      'collectionOffice': 'Newport'
+    },
+    {
+      'id': 'PEX0074586',
+      'name': 'Brown Schneider',
+      'collectionDate': '23/12/2018',
+      'collectionTime': '02:40',
+      'collectionOffice': 'London'
+    },
+    {
+      'id': 'PEX0722849',
+      'name': 'Russell Black',
+      'collectionDate': '27/12/2018',
+      'collectionTime': '09:39',
+      'collectionOffice': 'Peterborough'
+    },
+    {
+      'id': 'PEX6332932',
+      'name': 'Maggie Mckay',
+      'collectionDate': '23/12/2018',
+      'collectionTime': '10:31',
+      'collectionOffice': 'Belfast'
+    },
+    {
+      'id': 'PEX2310110',
+      'name': 'Allison Clements',
+      'collectionDate': '28/12/2018',
+      'collectionTime': '01:24',
+      'collectionOffice': 'Newport'
+    },
+    {
+      'id': 'PEX6740922',
+      'name': 'Reyes Richard',
+      'collectionDate': '21/12/2018',
+      'collectionTime': '05:12',
+      'collectionOffice': 'Glasgow'
+    },
+    {
+      'id': 'PEX9705615',
+      'name': 'Beatrice Blevins',
+      'collectionDate': '27/12/2018',
+      'collectionTime': '01:10',
+      'collectionOffice': 'London'
+    }
+  ],
+
+  'reports': [{
+      'date': '29/12/2018',
+      'name': '29/12/2018-Peterborough.xml'
+    },
+    {
+      'date': '24/12/2018',
+      'name': '29/12/2018-Peterborough.xml'
+    },
+    {
+      'date': '30/12/2018',
+      'name': '29/12/2018-Liverpool.xml'
+    },
+    {
+      'collectionOffice': 'Newport',
+      'date': '30/12/2018',
+      'name': '29/12/2018-Newport.xml'
+    },
+    {
+      'date': '30/12/2018',
+      'name': '29/12/2018-Liverpool.xml'
+    },
+    {
+      'collectionOffice': 'Newport',
+      'date': '30/12/2018',
+      'name': '29/12/2018-Newport.xml'
+    },
+    {
+      'date': '24/12/2018',
+      'name': '29/12/2018-Peterborough.xml'
+    },
+    {
+      'date': '24/12/2018',
+      'name': '29/12/2018-Belfast.xml'
+    }
+  ]
 }
--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const router = express.Router()
-
 // Add your routes here - above the module.exports line
 
-router.get('/', function (req, res) {
-  res.render('index')
+router.get('/v1/login', function (req, res) {
+  res.render('v1/login')
+})
+
+router.post('/v1/login', function (req, res) {
+  res.redirect('/v1/index')
+})
+
+router.get('/v1/index', function (req, res) {
+  console.log(req.session.data)
+  res.render('v1/index', {
+    newApplications: req.session.data.newApplications,
+    user: req.session.data.username
+  })
+})
+
+router.get('/v1/archive', function (req, res) {
+  console.log(req.session.data)
+  res.render('v1/archive', {
+    newApplications: req.session.data.newApplications,
+    user: req.session.data.username
+  })
+})
+
+router.get('/v1/reports', function (req, res) {
+  console.log(req.session.data)
+  res.render('v1/reports', {
+    reports: req.session.data.reports,
+    user: req.session.data.username
+  })
 })
 
 module.exports = router
