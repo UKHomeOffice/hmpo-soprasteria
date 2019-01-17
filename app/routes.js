3900fc9eee665581e6e489b57deb8c37a1f931f2
hvekriya <h.vekriya@kainos.com>
2019-01-17 10:59:32 +0000

    Updated the flow and added in location in header

--- app/routes.js
+++ app/routes.js
@@ -11,26 +11,37 @@ router.post('/v1/login', function (req, res) {
 })
 
 router.get('/v1/index', function (req, res) {
-  console.log(req.session.data)
   res.render('v1/index', {
     newApplications: req.session.data.newApplications,
-    user: req.session.data.username
+    user: req.session.data.username || 'user'
+  })
+})
+
+router.post('/v1/index', function (req, res) {
+  res.redirect('/v1/applications')
+})
+
+router.get('/v1/applications', function (req, res) {
+  res.render('v1/applications', {
+    location: req.session.data.office,
+    newApplications: req.session.data.newApplications,
+    user: req.session.data.username || 'user'
   })
 })
 
 router.get('/v1/archive', function (req, res) {
-  console.log(req.session.data)
   res.render('v1/archive', {
+    location: req.session.data.office,
     newApplications: req.session.data.newApplications,
-    user: req.session.data.username
+    user: req.session.data.username || 'user'
   })
 })
 
 router.get('/v1/reports', function (req, res) {
-  console.log(req.session.data)
   res.render('v1/reports', {
+    location: req.session.data.office,
     reports: req.session.data.reports,
-    user: req.session.data.username
+    user: req.session.data.username || 'user'
   })
 })
 
